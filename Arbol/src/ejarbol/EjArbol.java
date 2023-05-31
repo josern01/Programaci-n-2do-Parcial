c414b35e3b619f5b7e264dcf60bff07e3ce731c9
José RN <125505426+josern01@users.noreply.github.com>
2023-05-31 10:53:41 -0600

    Add files via upload

---
+++ Arbol/src/ejarbol/EjArbol.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejarbol;
+
+/**
+ *
+ * @author eq14
+ */
+public class EjArbol {
+    
+}
